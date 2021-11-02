a1644640f0831ba61d7ed973f9668b6d622294ad
Justin Forgue <26842354+bandogora@users.noreply.github.com>
2021-11-02 12:45:29 -0400

    make dir in test prep and Readme

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # basicray
+
 A ray tracer based on [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) written in verilog
 
 ## Testing equivalence
+
 To run the equivalence tests first run
 
 ```bash
 bash test_prep.sh
 ```
+
 Then run
 
 ```bash
--- test_prep.sh
+++ test_prep.sh
@@ -3,6 +3,7 @@
 rm ~/basicray/main ~/basicray/intersect_tests/run_tbs.sh
 rm ~/basicray/intersect_tests/results_c ~/basicray/intersect_tests/results_v
 rm -r ~/basicray/intersect_tests/tbs/
+mkdir ~/basicray/intersect_tests/tbs/
 g++ -o ~/basicray/main ~/basicray/main.cpp
 ./main >> ~/basicray/intersect_tests/results_c
 chmod 777 ~/basicray/intersect_tests/run_tbs.sh
